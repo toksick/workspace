27a4500bf87c7a2c63e2f142dbc3539cadbca927
Alex <toksick89@gmail.com>
2015-05-22 16:06:38 +0200

    init

---
+++ algo-intro/fastway.c
@@ -0,0 +1,14 @@
+#include <*>
+#include <strings.h>
+#include <stdlib.h>
+
+int fastrek(){
+
+}
+
+int main(int argc, char **argv) {
+	int band1 = {7,9,3,4,8,4};
+	int band2 = {8,5,6,4,5,7};
+	int t1 = {
+
+}
